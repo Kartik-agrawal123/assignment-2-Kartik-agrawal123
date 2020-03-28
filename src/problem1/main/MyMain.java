33fce6c17ae66457efb434ae9bc115fcc86691a9
Kartik-agrawal123 <Kartik.agrawal_cs18@gla.ac.in>
2020-03-28 12:00:45 +0530

    Main class Created

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -8,4 +8,19 @@ package problem1.main;
 // executable class
 
 public class MyMain {
+    public static void main(String[] args) {
+        MyBinarySearchTree bst = new MyBinarySearchTree();
+        bst.insert(100);
+        bst.insert(50);
+        bst.insert(150);
+        bst.insert(75);
+        bst.insert(25);
+        bst.insert(125);
+        bst.insert(175);
+        bst.traverse(bst.getRoot());
+
+
+
+
+    }
 }
--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -5,6 +5,57 @@
  *  Time: 7:17 PM
  */
 package problem1.mybst;
+import problem1.node.TreeNode;
 // to implement BinarySearchTree
 public class MyBinarySearchTree {
+    TreeNode root;
+
+    public TreeNode getRoot(){
+        return root;
+    }
+
+    //insert
+    public void insert(int data){
+        TreeNode node=new TreeNode(data);
+        if(root==null){
+            root=node;
+        }
+        else{
+            TreeNode temp=root;
+            TreeNode parent=null;
+            while(temp!=null){
+                parent=temp;
+                if(node.getData()<=temp.getData()){
+                    temp=temp.getLeft();
+                }
+                else{
+                    temp=temp.getRight();
+                }
+            }
+            if(node.getData()<=parent.getData()){
+                parent.setLeft(node);
+            }
+            else{
+                parent.setRight(node);
+            }
+        }
+
+    }
+
+    public void traversePreOrder(TreeNode node){
+        if(node != null){
+            System.out.print(node.getData()+" ");
+            traversePreOrder(node.getLeft());
+            traversePreOrder(node.getRight());
+
+        }
+    }
+
+    public void traversePostOrder(TreeNode node){
+        if(node != null){
+            traversePostOrder(node.getLeft());
+            traversePostOrder(node.getRight());
+            System.out.print(node.getData()+" ");
+        }
+    }
 }
