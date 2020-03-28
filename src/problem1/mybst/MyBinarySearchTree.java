20c1c7360fdf70383fbadb24b9a12038cd00a44d
Kartik-agrawal123 <Kartik.agrawal_cs18@gla.ac.in>
2020-03-28 12:21:59 +0530

    my bst create

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -5,57 +5,90 @@
  *  Time: 7:17 PM
  */
 package problem1.mybst;
+
 import problem1.node.TreeNode;
+
 // to implement BinarySearchTree
 public class MyBinarySearchTree {
     TreeNode root;
+    int count;
 
-    public TreeNode getRoot(){
+    public MyBinarySearchTree() {
+        root = null;
+        count = 0;
+    }
+
+    public void insert(int data) {
+        TreeNode ns = new TreeNode();
+        ns.setdata(data);
+        if (root == null) {
+            root = ns;
+        } else {
+            TreeNode current = root;
+            while (true) {
+                if (data > current.getdata()) {
+                    if (current.getRight() == null) {
+                        current.setRight(ns);
+                        break;
+                    } else {
+                        current = current.getRight();
+                    }
+                } else {
+                    if (current.getLeft() == null) {
+                        current.setLeft(ns);
+                        break;
+                    } else {
+                        current = current.getLeft();
+                    }
+                }
+            }
+        }
+    }
+
+    private void LeftNode(TreeNode root) {
+        if (root == null) return;
+        else {
+            if (root.getLeft() != null) System.out.println(root.getLeft().getdata());
+            else count++;
+            LeftNode(root.getLeft());
+            LeftNode(root.getRight());
+        }
+    }
+
+    public void printLeftNode() {
+        LeftNode(root);
+    }
+
+    public void countNotLeft() {
+        System.out.println(count);
+    }
+
+    public void preorder(TreeNode root) {
+        if (root == null) return;
+        else {
+            System.out.print(root.getdata() + "\t");
+            preorder(root.getLeft());
+            preorder(root.getRight());
+
+        }
+    }
+
+    public void postorder(TreeNode root) {
+        if (root == null) return;
+        else {
+            postorder(root.getLeft());
+            postorder(root.getRight());
+            System.out.print(root.getdata() + "\t");
+        }
+    }
+
+    public void preVSpost() {
+        postorder(root);
+        System.out.println();
+        preorder(root);
+    }
+
+    public TreeNode getroot() {
         return root;
-    }
-
-    //insert
-    public void insert(int data){
-        TreeNode node=new TreeNode(data);
-        if(root==null){
-            root=node;
-        }
-        else{
-            TreeNode temp=root;
-            TreeNode parent=null;
-            while(temp!=null){
-                parent=temp;
-                if(node.getData()<=temp.getData()){
-                    temp=temp.getLeft();
-                }
-                else{
-                    temp=temp.getRight();
-                }
-            }
-            if(node.getData()<=parent.getData()){
-                parent.setLeft(node);
-            }
-            else{
-                parent.setRight(node);
-            }
-        }
-
-    }
-
-    public void traversePreOrder(TreeNode node){
-        if(node != null){
-            System.out.print(node.getData()+" ");
-            traversePreOrder(node.getLeft());
-            traversePreOrder(node.getRight());
-
-        }
-    }
-
-    public void traversePostOrder(TreeNode node){
-        if(node != null){
-            traversePostOrder(node.getLeft());
-            traversePostOrder(node.getRight());
-            System.out.print(node.getData()+" ");
-        }
     }
 }
